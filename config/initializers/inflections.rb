b0d46902d4bc8f83a16cacc09eef7d17fdcdcef0
Ivan Ali Rodriguez Vasquez <ali.rodri.vasquez@gmail.com>
2019-11-01 13:08:18 -0600

    Organizacion de personajes
    Se crea la tabla de las organizaciones de los personajes.

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -6,7 +6,9 @@
 ActiveSupport::Inflector.inflections(:en) do |inflect|
   #inflect.plural /^(ox)$/i, '\1en'
   #inflect.singular /^(ox)en/i, '\1'
-  #inflect.irregular 'person', 'people'
+  inflect.irregular 'character_organization', 'characters_organizations'
+  inflect.irregular 'CharacterOrganization', 'CharactersOrganizations'
+
   inflect.uncountable %w( rol )
 end
 
