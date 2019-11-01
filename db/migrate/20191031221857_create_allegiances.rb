9ace5a7c7cbe6b86e977cb9207e017983c2dcf8d
Ivan Ali Rodriguez Vasquez <ali.rodri.vasquez@gmail.com>
2019-11-01 11:39:35 -0600

    Subir archivo faltante
    Se sube el archivo faltante con respecto a lealtad.

--- db/migrate/20191031221857_create_allegiances.rb
+++ db/migrate/20191031221857_create_allegiances.rb
@@ -2,9 +2,9 @@ class CreateAllegiances < ActiveRecord::Migration[5.2]
   def change
     create_table :allegiances do |t|
       t.string :name
-      t.timestamp :deleted_at
 
       t.timestamps
+      t.timestamp :deleted_at
     end
     add_index :allegiances, :deleted_at
 
